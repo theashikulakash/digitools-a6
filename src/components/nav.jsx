40a78d6e1d90c91ca77d702a58d7d234416aa810
Md Ashikul Islam <akashashikul@gmail.com>
2026-03-30 14:31:52 +0600

    talwindcss and daisyui added

---
+++ src/components/nav.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ShoppingCart } from 'lucide-react';
+
+const Nav = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Products</a></li>
+        <li><a>Features</a></li>
+        <li><a>Pricing</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>FAQ</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Products</a></li>
+        <li><a>Features</a></li>
+        <li><a>Pricing</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>FAQ</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end gap-4">
+    <ShoppingCart className='cursor-pointer'></ShoppingCart>
+    <a className="cursor-pointer">Login</a>
+        <a className="btn font-bold bg-[#4F39F6] text-white rounded-3xl hover:border-2 hover:border-[#4F39F6] hover:bg-white hover:text-[#4F39F6]">Get Started</a>
+  </div>
+</div>
+    );
+};
+
+export default Nav;
